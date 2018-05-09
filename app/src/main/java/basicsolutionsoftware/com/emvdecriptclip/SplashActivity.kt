1c26656002e205c00b6ff623d3e1815699727ae8
Erick Daniel Juarez Gil <erickdanieljuarezgil@gmail.com>
2018-05-09 10:04:42 -0500

    First Commit

---
+++ app/src/main/java/basicsolutionsoftware/com/emvdecriptclip/SplashActivity.kt
@@ -0,0 +1,12 @@
+package basicsolutionsoftware.com.emvdecriptclip
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
