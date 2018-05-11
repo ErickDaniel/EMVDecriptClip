f7203254292932617405e72de8c9f2cf13e5263b
Erick Daniel Juarez Gil <erickdanieljuarezgil@gmail.com>
2018-05-11 11:43:26 -0500

    Multiple changes
    - [ NE ]Styles
    - [ NE ]Colors
    - [ NE ]MainActivity
    - [ NE ]Logo
    - [ UP ]SplashActivity

--- app/src/main/java/basicsolutionsoftware/com/emvdecriptclip/SplashActivity.kt
+++ app/src/main/java/basicsolutionsoftware/com/emvdecriptclip/SplashActivity.kt
@@ -1,12 +1,28 @@
 package basicsolutionsoftware.com.emvdecriptclip
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.os.CountDownTimer
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        startCountDown()
     }
+
+    fun startCountDown(){
+        object : CountDownTimer(2000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+
+            }
+        }.start()
+    }
+
 }
